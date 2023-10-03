b0544971f47826e388af6548a907c727131dfbf6
Laurore83 <laurore83@yahoo.fr>
2023-10-03 11:00:02 +0200

    fait

---
+++ index.js
@@ -0,0 +1,11 @@
+import chalk from "chalk";
+
+const name1 = "adam";
+const name2 = "victor";
+const name3 = "david";
+const name4 = "lucas";
+
+
+
+
+console.log (chalk.blue(name1, name2, name3, name4));
